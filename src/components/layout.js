cf8b93075ad3dfb5af9315058dd2855159cf0c2c
kevin <kevin.berclouw@t-mobile.cz>
2020-04-19 16:27:02 +0200

    customize delog metadata

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
-import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
-import Navigation from "../components/navigation"
-import 'prismjs/themes/prism-okaidia.css';
+import React from 'react'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import Navigation from '../components/navigation'
+import 'prismjs/themes/prism-okaidia.css'
 
 export default ({ children }) => {
   const data = useStaticQuery(
@@ -16,16 +16,25 @@ export default ({ children }) => {
     `
   )
   return (
-    <div className="site-wrapper">
-      <header className="site-header">
-        <div className="site-title">
-          <Link to="/">{data.site.siteMetadata.title}</Link>
+    <div className='site-wrapper'>
+      <header className='site-header'>
+        <div className='site-title'>
+          <Link to='/'>{data.site.siteMetadata.title}</Link>
         </div>
         <Navigation />
       </header>
       {children}
-      <footer className="site-footer">
-        <p>&copy; 2020 Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
+      <footer className='site-footer'>
+        <p>
+          &copy; 2020{' '}
+          <span role='img' aria-label='love'>
+            ❤️
+          </span>{' '}
+          Zuzana Senkyrka and{' '}
+          <a href='https://kevinsisland.netlify.com/'>Clawz Laboratories</a>{' '}
+          &bull; Delog starter
+          crafted by <a href='https://w3layouts.com'>W3Layouts</a>
+        </p>
       </footer>
     </div>
   )
